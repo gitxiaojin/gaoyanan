589aad7fcb7b94cf99cc96e9a031d9925116ccfb
xiaojin <email_xiaojin@163.com>
2018-11-22 23:19:59 +0800

    Scripts

--- Scripts/my.js
+++ Scripts/my.js
@@ -6,31 +6,28 @@ $(function(){
         $(this).addClass('current').siblings().removeClass('current');
 		//alert($(this).index());
 		var jump = $('.louCeng').eq($(this).index()).offset().top;
+		// 获取每部分到顶部的距离
 		$('body,html').stop().animate({'scrollTop':''+jump+'px'},300);
     });
-	
-	function myFn(par1){
-		$('.navright ul li').eq(par1).addClass('current').siblings().removeClass('current');
+  })
+//切换tab
+$('.webPage h2 span').click(function(event) {
+	$(this).addClass('current').siblings().removeClass('current')
+	var num=$(this).index()
+	$('.webPageCont li').eq(num).show().siblings().hide()
+});
+// 折叠显示
+ var onOff=true
+$('.iconCollaspe').click(function(event) {
+	if(onOff==true){
+		$('.navright').css("left","0")
+		onOff=false
+	}else{
+		$('.navright').css("left","-30%")
+		onOff=true
 	}
-	$(window).scroll(function(e) {
-		//判断滚动距离如果大于第几层距离顶部的距离，那么就让第几个li添加特殊current
-		var sT = $(window).scrollTop();
-		if(sT >= $('.louCeng').eq(5).offset().top){
-			myFn(5);
-		}else if(sT >= $('.louCeng').eq(4).offset().top){
-			myFn(4);
-		}else if(sT >= $('.louCeng').eq(3).offset().top){
-			myFn(3);
-		}else if(sT >= $('.louCeng').eq(2).offset().top){
-			myFn(2);
-		}else if(sT >= $('.louCeng').eq(1).offset().top){
-			myFn(1);
-		}else if(sT >= $('.louCeng').eq(0).offset().top){
-			myFn(0);
-		}
-    });
-})
-
+	
+});
 
 //欢迎页
 $(function(){
@@ -41,47 +38,7 @@ $(function(){
 		}, function() {
 			$('.tip').stop().animate({'bottom': '8%', 'opacity': '0'}, 300);
 		});
-	//banner图
-$(function(){
-	var sx=0;
-	var timer=null
 	
-	var colors=['#050214','#000','#43a3f9',]
-	$('.banner ul li').each(function(index, element) {
-        $(element).css('background-color',colors[index])
-    });
-	
-	$('.banner .arrright').click(function(e) {
-        sx++;
-		if(sx>2){sx=0}
-		$('.banner ul').stop().animate({left:-sx * 100+'%' },500)
-    });
-	$('.banner .arrleft').click(function(e) {
-        sx--;
-		if(sx<0){sx=2}
-		$('.banner ul').stop().animate({left:-sx * 100+'%' },500)
-    });
-	
-	//自动播放模块
-	timer=setInterval(autoplay,2000)
-	function autoplay(){
-		sx++
-		if(sx>2){sx=0}
-		
-		$('.banner ul').stop().animate({left:-sx * 100+'%' },500)
-	}
-	
-	//鼠标移上停止定时器模块
-	$('.banner').hover(function(e) {
-        //停止自动播放，清除定时器；
-		clearInterval(timer)
-		$('.banner span').fadeIn();
-    },function(e){
-		clearInterval(timer)
-		timer=setInterval(autoplay,2000)
-		$('.banner span').fadeOut()
-    });
-})
 	})
 	$(function(){
 		$('.workout').hover(function(e) {
@@ -96,7 +53,7 @@ $(function(){
 		$('.Webclick span.closeAll').click(function(e) {
                 $('.Webclick').fadeOut(500) 
 				$("body,html").css("overflow","auto")
-            }); 
+        }); 
 			
 	})
 
@@ -105,14 +62,10 @@ $(function(){
 	$('.foldmain li').mouseover(function(e) {
         $(this).stop().animate({width:1000},500).siblings().stop().animate({width:23},500)
     }).mouseout(function(e) {
-        $('.foldmain li').stop().animate({width:163},500)
+        $('.foldmain li').stop().animate({width:143},500)
     });
 })
-
-
-
-//App滚动
-window.onload=function() {
+$(function(){
 	//全局变量
 	// APP异性滚动业务开始
 	var json0 = {"width":"145px","height":"343.434898px","left":"-180px","top":"179px"};
@@ -182,6 +135,7 @@ window.onload=function() {
 				}
 			}
 		}
+
 	);
 	// APP异性滚动业务结束
-}
+})
